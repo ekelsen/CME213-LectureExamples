a0f4c36d2f52b5de33a13f2ad1c9c85a5d7b00f6
Erich Elsen <erich@royal-caliber.com>
2013-04-15 15:00:35 +0800

    added error checking

--- Lecture1/segfault.cu
+++ Lecture1/segfault.cu
@@ -1,3 +1,5 @@
+#include "../utils.h"
+
 __global__
 void myKernel(int *in) {
     in[threadIdx.x] += 1;
@@ -5,8 +7,10 @@ void myKernel(int *in) {
 
 int main(void) {
     int *dIn;
-    cudaMalloc(&dIn, sizeof(int));
+    checkCudaErrors(cudaMalloc(&dIn, sizeof(int)));
 
     myKernel<<<1, 128>>>(dIn);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
     return 0;
 }
