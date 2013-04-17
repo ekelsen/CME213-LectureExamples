73c3629357ef87297c09db428f58f2aebac912ad
Erich Elsen <erich@royal-caliber.com>
2013-04-17 09:08:05 -0700

    made error message less spammy

--- Lecture1/segfault.cu
+++ Lecture1/segfault.cu
@@ -9,7 +9,7 @@ int main(void) {
     int *dIn;
     checkCudaErrors(cudaMalloc(&dIn, sizeof(int)));
 
-    myKernel<<<1, 128>>>(dIn);
+    myKernel<<<1, 2>>>(dIn);
     cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
     return 0;
