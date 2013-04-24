24cc0b4d637ec173153d555de2289568c5755ae8
Erich Elsen <ekelsen@gmail.com>
2013-04-23 19:51:24 -0700

    added denormal test

--- Lecture3/fp_associativity.cpp
+++ Lecture3/fp_associativity.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <iomanip>
+#include <limits>
+#include <xmmintrin.h>
+
+void enableFtzDaz()
+{
+  int mxcsr = _mm_getcsr ();
+
+  mxcsr |= (1<<15) | (1<<11);
+  mxcsr |= (1<<6);
+
+  _mm_setcsr (mxcsr);
+}
 
 int main(void) {
   double a = .1;
@@ -17,6 +29,20 @@ int main(void) {
   
   if (sum2 == .6)
     std::cout << ".1 + (.2 + .3) == .6" << std::endl;
+
+  float minf = std::numeric_limits<float>::min();
+  float smallf = 1.01f * minf;
+
+  std::cout << "\n\n";
+  std::cout << "Val1: " << std::setprecision(20) << smallf << std::endl;
+  std::cout << "Val2: " << std::setprecision(20) << minf   << std::endl;
+  std::cout << "With denormals: " << std::endl;
+  std::cout << "Val1 - Val2: " << smallf - minf << std::endl;
+
+  enableFtzDaz();
+
+  std::cout << std::endl << "Without denormals: " << std::endl;
+  std::cout << "Val1 - Val2: " << smallf - minf << std::endl;
 
   return 0;
 }
